7991babd6a6579cb4464a12433533627997815da
Pravish Misra <pravishmisra311@gmail.com>
2026-06-21 11:03:16 +0530

    Added day6

---
+++ day6/Linkedlist.java
@@ -0,0 +1,41 @@
+package day6;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class LinkedList{
+    Node head;
+
+    void addFirst(int data) {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+        public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        list.addFirst(30);
+        list.addFirst(20);
+        list.addFirst(10);
+
+        list.display();
+    }
+}
+
+
